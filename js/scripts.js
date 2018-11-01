045ef1b37ceca2b618616ed2aeba3f6d3dc1ae71
Chris Rudnicky and Noah Schooler <noah@>
2018-11-01 11:58:55 -0700

    add CSS styling, disable all buttons once a player has won

--- js/scripts.js
+++ js/scripts.js
@@ -6,22 +6,13 @@ function Player() {
 
 Player.prototype.hold = function() {
   this.totalScore += this.score
-  playerOne.scoreChecker()
-  playerTwo.scoreChecker()
   this.score = 0
-}
-
-Player.prototype.scoreChecker = function() {
-  if (playerOne.totalScore >= 100) {
-    alert("Congratulations Player One, you have has won!")
-  } else if (playerTwo.totalScore >= 100) {
-    alert("Congratulations Player Two, you have won!")
-  }
 }
 
 Player.prototype.roll = function(turn) {
   var number = Math.floor((Math.random() * 6) + 1)
-  $("#roll-number").text(number)
+  console.log(number)
+  // $("#roll-number").text(number)
   if (turn === "PlayerOne") {
     if (number === 1) {
       playerOne.score = 0
@@ -35,24 +26,34 @@ Player.prototype.roll = function(turn) {
       playerTwo.score += number
     }
   }
+  return number
 }
 
 //User interface logic
 var playerOne = new Player();
 var playerTwo = new Player();
 
-function playerOneContols() {
+function playerOneControls() {
   $("#player-one-roll").click(function() {
-    var result = playerOne.roll("PlayerOne")
+    playerOne.roll("PlayerOne")
+    $("#roll-number").text(playerOne.roll())
     $("#turn-score").text(playerOne.score)
-    if (playerOne.score === 0) {
+    if (playerOne.totalScore += playerOne.score >= 10) {
+      var buttons = document.getElementsByClassName("test-button");
+      for(var i = 0; i < buttons.length; i++) {
+      buttons[i].disabled = true;
+      }
+      $("#roll-number").text("Congratulations Player One, you have has won!")
+    } else if (playerOne.score === 0) {
       document.getElementById("player-one-roll").disabled = true;
       document.getElementById("player-two-roll").disabled = false;
       $("#roll-number").text("You rolled a 1. It's player two's turn.")
     }
   });
   $("#player-one-hold").click(function() {
-    var result = playerOne.hold()
+    playerOne.hold()
+    console.log(playerOne.totalScore);
+
     $("#total-score").text(playerOne.totalScore)
     $("#turn-score").text(playerOne.score)
     document.getElementById("player-one-roll").disabled = true;
@@ -63,25 +64,33 @@ function playerOneContols() {
 }
 function playerTwoControls() {
   $("#player-two-roll").click(function() {
-    var result = playerTwo.roll("PlayerTwo")
+    playerTwo.roll("PlayerTwo")
+    $("#roll-number").text(playerTwo.roll())
     $("#player-two-turn-score").text(playerTwo.score)
-    if (playerTwo.score === 0) {
+    if (playerTwo.totalScore += playerTwo.score >= 10) {
+      var buttons = document.getElementsByClassName("test-button");
+      for(var i = 0; i < buttons.length; i++) {
+      buttons[i].disabled = true;
+      }
+      $("#roll-number").text("Congratulations Player Two, you have has won!")
+    } else if (playerTwo.score === 0) {
       document.getElementById("player-two-roll").disabled = true;
       document.getElementById("player-one-roll").disabled = false;
       $("#roll-number").text("You rolled a 1. It's player one's turn.")
     }
   });
   $("#player-two-hold").click(function() {
-    var result = playerTwo.hold()
+    playerTwo.hold()
     $("#player-two-total-score").text(playerTwo.totalScore)
     $("#player-two-turn-score").text(playerTwo.score)
     document.getElementById("player-two-roll").disabled = true;
     document.getElementById("player-one-roll").disabled = false;
     $("#roll-number").text("It's player one's turn.")
+
   });
 }
 
 $(document).ready(function() {
-  playerOneContols();
+  playerOneControls();
   playerTwoControls();
 });
