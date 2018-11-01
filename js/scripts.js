27152b4e705ceebd6f5741e97e72b662b5c4ac41
Chris Rudnicky and Noah Schooler <chris@>
2018-11-01 14:32:24 -0700

    Add a button to reset the page and clean up smelly code

--- js/scripts.js
+++ js/scripts.js
@@ -11,8 +11,6 @@ Player.prototype.hold = function() {
 
 Player.prototype.roll = function(turn) {
   var number = Math.floor((Math.random() * 6) + 1)
-  console.log(number)
-  // $("#roll-number").text(number)
   if (turn === "PlayerOne") {
     if (number === 1) {
       playerOne.score = 0
@@ -35,47 +33,65 @@ var playerTwo = new Player();
 
 function playerOneControls() {
   $("#player-one-roll").click(function() {
-    playerOne.roll("PlayerOne")
-    $("#roll-number").text(playerOne.roll())
+    var number = playerOne.roll("PlayerOne")
+    $("#roll-number").text(number)
     $("#turn-score").text(playerOne.score)
-    if (playerOne.totalScore += playerOne.score >= 10) {
-      var buttons = document.getElementsByClassName("test-button");
+    var accumulated = playerOne.totalScore + playerOne.score
+    console.log(accumulated);
+    if (accumulated >= 20) {
+      $("#player-one").addClass("opaque")
+      $("#player-two").addClass("opaque")
+      var buttons = document.getElementsByClassName("move-button");
       for(var i = 0; i < buttons.length; i++) {
-      buttons[i].disabled = true;
+        buttons[i].disabled = true;
       }
-      $("#roll-number").text("Congratulations Player One, you have has won!")
+      $("#roll-number").text("Congratulations Player One, you have won!")
     } else if (playerOne.score === 0) {
+      $("#player-one").addClass("opaque")
+      $("#player-two").removeClass("opaque")
       document.getElementById("player-one-roll").disabled = true;
+      document.getElementById("player-one-hold").disabled = true;
       document.getElementById("player-two-roll").disabled = false;
+      document.getElementById("player-two-hold").disabled = false;
       $("#roll-number").text("You rolled a 1. It's player two's turn.")
     }
   });
   $("#player-one-hold").click(function() {
     playerOne.hold()
     console.log(playerOne.totalScore);
-
     $("#total-score").text(playerOne.totalScore)
     $("#turn-score").text(playerOne.score)
+    $("#player-one").addClass("opaque")
+    $("#player-two").removeClass("opaque")
     document.getElementById("player-one-roll").disabled = true;
+    document.getElementById("player-one-hold").disabled = true;
     document.getElementById("player-two-roll").disabled = false;
+    document.getElementById("player-two-hold").disabled = false;
     $("#roll-number").text("It's player two's turn.")
   });
-
 }
+
 function playerTwoControls() {
   $("#player-two-roll").click(function() {
-    playerTwo.roll("PlayerTwo")
-    $("#roll-number").text(playerTwo.roll())
+    var number = playerTwo.roll("PlayerTwo")
+    $("#roll-number").text(number)
     $("#player-two-turn-score").text(playerTwo.score)
-    if (playerTwo.totalScore += playerTwo.score >= 10) {
-      var buttons = document.getElementsByClassName("test-button");
+    var accumulated = playerOne.totalScore + playerOne.score
+    if (accumulated >= 20) {
+      $("#player-one").addClass("opaque")
+      $("#player-two").addClass("opaque")
+      var buttons = document.getElementsByClassName("move-button");
       for(var i = 0; i < buttons.length; i++) {
-      buttons[i].disabled = true;
+        buttons[i].disabled = true;
       }
-      $("#roll-number").text("Congratulations Player Two, you have has won!")
+      $("#roll-number").text("Congratulations Player Two, you have won!")
     } else if (playerTwo.score === 0) {
+      $("#player-two").addClass("opaque")
+      $("#player-one").removeClass("opaque")
       document.getElementById("player-two-roll").disabled = true;
+      document.getElementById("player-two-hold").disabled = true;
       document.getElementById("player-one-roll").disabled = false;
+      document.getElementById("player-one-hold").disabled = false;
       $("#roll-number").text("You rolled a 1. It's player one's turn.")
     }
   });
@@ -83,14 +99,20 @@ function playerTwoControls() {
     playerTwo.hold()
     $("#player-two-total-score").text(playerTwo.totalScore)
     $("#player-two-turn-score").text(playerTwo.score)
+    $("#player-two").addClass("opaque")
+    $("#player-one").removeClass("opaque")
     document.getElementById("player-two-roll").disabled = true;
+    document.getElementById("player-two-hold").disabled = true;
     document.getElementById("player-one-roll").disabled = false;
+    document.getElementById("player-one-hold").disabled = false;
     $("#roll-number").text("It's player one's turn.")
-
   });
 }
 
 $(document).ready(function() {
   playerOneControls();
   playerTwoControls();
+  $("#play-again").click(function() {
+    location.reload();
+  });
 });
