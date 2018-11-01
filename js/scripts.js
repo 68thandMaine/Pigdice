29a0d925a82f7eabd9f18f9105cb637b89e60dc0
Chris Rudnicky and Noah Schooler <chris@>
2018-11-01 10:24:22 -0700

    Add ui logic that determines whose turn it is and bootstrap  styling to the page

--- js/scripts.js
+++ js/scripts.js
@@ -4,24 +4,24 @@ function Player() {
   this.totalScore = 0
 }
 
-Player.prototype.hold = function () {
+Player.prototype.hold = function() {
   this.totalScore += this.score
   playerOne.scoreChecker()
   playerTwo.scoreChecker()
   this.score = 0
 }
 
-Player.prototype.scoreChecker = function () {
+Player.prototype.scoreChecker = function() {
   if (playerOne.totalScore >= 100) {
     alert("Congratulations Player One, you have has won!")
-  }
-  else if (playerTwo.totalScore >= 100 ){
+  } else if (playerTwo.totalScore >= 100) {
     alert("Congratulations Player Two, you have won!")
   }
 }
 
-Player.prototype.roll = function (turn) {
-  var number = Math.floor((Math.random() * 6 ) + 1)
+Player.prototype.roll = function(turn) {
+  var number = Math.floor((Math.random() * 6) + 1)
+  $("#roll-number").text(number)
   if (turn === "PlayerOne") {
     if (number === 1) {
       playerOne.score = 0
@@ -41,26 +41,47 @@ Player.prototype.roll = function (turn) {
 var playerOne = new Player();
 var playerTwo = new Player();
 
-$(document).ready(function(){
+function playerOneContols() {
   $("#player-one-roll").click(function() {
     var result = playerOne.roll("PlayerOne")
     $("#turn-score").text(playerOne.score)
+    if (playerOne.score === 0) {
+      document.getElementById("player-one-roll").disabled = true;
+      document.getElementById("player-two-roll").disabled = false;
+      $("#roll-number").text("You rolled a 1. It's player two's turn.")
+    }
   });
   $("#player-one-hold").click(function() {
     var result = playerOne.hold()
     $("#total-score").text(playerOne.totalScore)
     $("#turn-score").text(playerOne.score)
+    document.getElementById("player-one-roll").disabled = true;
+    document.getElementById("player-two-roll").disabled = false;
+    $("#roll-number").text("It's player two's turn.")
   });
 
+}
+function playerTwoControls() {
   $("#player-two-roll").click(function() {
     var result = playerTwo.roll("PlayerTwo")
     $("#player-two-turn-score").text(playerTwo.score)
+    if (playerTwo.score === 0) {
+      document.getElementById("player-two-roll").disabled = true;
+      document.getElementById("player-one-roll").disabled = false;
+      $("#roll-number").text("You rolled a 1. It's player one's turn.")
+    }
   });
   $("#player-two-hold").click(function() {
     var result = playerTwo.hold()
     $("#player-two-total-score").text(playerTwo.totalScore)
     $("#player-two-turn-score").text(playerTwo.score)
+    document.getElementById("player-two-roll").disabled = true;
+    document.getElementById("player-one-roll").disabled = false;
+    $("#roll-number").text("It's player one's turn.")
   });
+}
 
-
+$(document).ready(function() {
+  playerOneContols();
+  playerTwoControls();
 });
